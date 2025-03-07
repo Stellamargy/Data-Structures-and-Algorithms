defe09e6ab1acfa53f861ed8d07e532a840507e4
stlmargy <stellahmargy0211@gmail.com>
2025-03-07 10:25:53 +0300

    adds deleteItemByIndex method - deletes item by passing index

--- array.js
+++ array.js
@@ -11,34 +11,48 @@ class myArray {
     return this.data;
   }
 
-  get(index){
-    return this.data[index]
+  get(index) {
+    return this.data[index];
   }
 
-  pop(){
-    const poppedItem=delete this.data[this.length-1]
-    this.length--
-    return poppedItem
+  pop() {
+    const poppedItem = delete this.data[this.length - 1];
+    this.length--;
+    return poppedItem;
   }
 
-  shift(){
+  shift() {
     const firstItem = delete this.data[0];
-    this.length--
-    return this.data = Object.fromEntries(
-      Object.entries(this.data).map(([key, value]) => [(key-1), value])
-    );
-
+    this.decrementLength();
+    const data = this.updatesKeys();
+    return data
   }
 
+  updatesKeys() {
+    return this.data = Object.fromEntries(
+      Object.entries(this.data).map(([key, value]) => [key - 1, value])
+    );
+  }
+
+  decrementLength() {
+    this.length--;
+  }
+
+  deleteItemByIndex(index) {
+    const item = delete this.data[index];
+    this.updatesKeys();
+    this.decrementLength()
+    return item;
+  }
 }
 
 const newArr = new myArray();
 newArr.push("apples");
 newArr.push("mangoes");
 newArr.push("kiwi");
-console.log(newArr) ;
-console.log(newArr.length) ;
-console.log(newArr.shift()) ;
-console.log(newArr.length) ;
-
-
+console.log(newArr);
+console.log(newArr.length);
+console.log(newArr.shift());
+console.log(newArr.length);
+console.log(newArr.deleteItemByIndex(0));
+console.log(newArr);
