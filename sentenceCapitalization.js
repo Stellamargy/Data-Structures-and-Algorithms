2961956d51d978c6ae6a0677d1e630d52328a6ac
stlmargy <stellahmargy0211@gmail.com>
2025-03-07 13:09:43 +0300

    converts the string to lowercase first

--- sentenceCapitalization.js
+++ sentenceCapitalization.js
@@ -1,2 +1,2 @@
-const sentenceCapitalization=(str)=>str.split(' ').map(item=>item.charAt(0).toUpperCase()+item.slice(1)).join(" ");
+const sentenceCapitalization=(str)=>str.lowercase().split(' ').map(item=>item.charAt(0).toUpperCase()+item.slice(1)).join(" ");
 console.log(sentenceCapitalization("hi, my name is stella margy"))
