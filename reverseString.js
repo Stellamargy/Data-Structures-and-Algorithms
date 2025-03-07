f7b1f3df94e196b155e03f47fc2bf855bc3b89a0
stlmargy <stellahmargy0211@gmail.com>
2025-03-07 11:34:16 +0300

    reverse string function

--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,18 @@
-function reverseString(str) {
-  let reversedString = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    const character = str.charAt(i);
-    reversedString = reversedString + character;
-  }
-  return reversedString;
-}
-console.log(reverseString("Margy"));
+// function reverseString(str) {
+//   let reversedString = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     const character = str.charAt(i);
+//     reversedString = reversedString + character;
+//   }
+//   return reversedString;
+// }
+// console.log(reverseString("Hello"));
+
+
+// convert the string to an array  using split 
+// reverse the array using reverse() ,
+// join the items in the array using join ()
+
+const reversedString = (str)=>str.split("").reverse().join("")
+console.log(reversedString("Stella"))
+
