a0e2922295102b151e57831dfa1ca568e15783eb
stlmargy <stellahmargy0211@gmail.com>
2025-03-07 11:21:25 +0300

    code formatting

--- reverseString.js
+++ reverseString.js
@@ -1,14 +1,9 @@
-function reverseString(str){
-    let reversedString=""
-    for(let i=(str.length-1) ; i>=0 ; i--){
-        const character=str.charAt(i)
-        reversedString = reversedString + character
-        // console.log(reversedString)
-       
-    }
-    
-    // console.log(reversedString)
-    return reversedString
-
+function reverseString(str) {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    const character = str.charAt(i);
+    reversedString = reversedString + character;
+  }
+  return reversedString;
 }
-console.log(reverseString("Margy"))
+console.log(reverseString("Margy"));
